2d8ff5b5d6a0ef383dfe8ae770222c5e936955a1
Kai Chang <kaijchang@gmail.com>
2019-01-03 21:52:46 -0800

    use abs path

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+import os
+
 setup(
-    ext_modules=cythonize('percent_white_pixels.pyx')
+    ext_modules=cythonize(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'percent_white_pixels.pyx'
+        )
+    )
 )
